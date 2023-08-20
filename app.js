27df2bd0552004ffb43bff764936a1ed658639a4
briancoco <briannguyencoco@gmail.com>
2023-08-20 16:08:40 -0500

    page now being served by node web server

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+app.use(express.static('./static'));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
